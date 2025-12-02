ef65d78b55a09ae578d636ba40e6e24dc8a36f9b
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-02 12:02:44 +0100

    new point try

--- point/main.go
+++ point/main.go
@@ -1,42 +1,57 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
 	y int
 }
 
-func setPoint(p *point) {
-	p.x = 42
-	p.y = 21
-}
-
-func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-}
-
-func printNbr(n int) {
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-
-	if n >= 10 {
-		printNbr(n / 10)
-	}
-	z01.PrintRune(rune(n%10 + '0'))
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
 }
 
 func main() {
-	p := &point{}
-	setPoint(p)
+	points := &point{}
 
+	setPoint(points)
 	printStr("x = ")
-	printNbr(p.x)
+	IntoRune(points.x)
 	printStr(", y = ")
-	printNbr(p.y)
+	IntoRune(points.y)
 	z01.PrintRune('\n')
+}
+
+func printStr(s string) {
+	for _, v := range s {
+		z01.PrintRune(v)
+	}
+}
+
+func checkAndPrint(r int) {
+	c := '0'
+	if r == 0 {
+		z01.PrintRune(c)
+		return
+	}
+	for i := 1; i <= r%10; i++ {
+		c++
+	}
+	for i := -1; i >= r%10; i-- {
+		c++
+	}
+	if r/10 != 0 {
+		checkAndPrint(r / 10)
+	}
+	z01.PrintRune(c)
+}
+
+func IntoRune(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	checkAndPrint(n)
 }
