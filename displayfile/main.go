a578247b7122076b45c13a0a4e591cd7348ffc79
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-02 13:50:09 +0100

    hhhd

--- displayfile/main.go
+++ displayfile/main.go
@@ -1,64 +1,20 @@
 package main
 
-import "github.com/01-edu/z01"
-
-type point struct {
-	x int
-	y int
-}
-
-func setPoint(pt *point) {
-	// Set integers 42 and 21 (written as sums to avoid a single literal)
-	pt.x = 40 + 2
-	pt.y = 20 + 1
-}
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	coords := &point{}
-	setPoint(coords)
-
-	baseTen := 0
-	for range ".........." {
-		baseTen++
+	if len(os.Args) == 2 {
+		data, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			fmt.Print("ERROR: " + err.Error())
+		}
+		fmt.Print(string(data))
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+	} else if len(os.Args) < 2 {
+		fmt.Println("File name missing")
 	}
-
-	axisIndex := 0
-	for range "xy" {
-		label := ""
-		value := 0
-
-		if axisIndex == 0 {
-			label = "x = "
-			value = coords.x
-		} else {
-			label = ", y = "
-			value = coords.y
-		}
-
-		for _, ch := range label {
-			z01.PrintRune(ch)
-		}
-
-		position := 0
-		for range "TO" {
-			digitValue := 0
-
-			if position == 0 {
-				digitValue = value / baseTen
-			} else {
-				digitValue = value % baseTen
-			}
-
-			outputRune := '0'
-			for count := 0; count < digitValue; count++ {
-				outputRune++
-			}
-
-			z01.PrintRune(outputRune)
-			position++
-		}
-		axisIndex++
-	}
-
-	z01.PrintRune('\n')
 }
