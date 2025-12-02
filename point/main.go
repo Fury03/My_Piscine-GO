bbc7d743af0cb146b12ca67a7cbb27274f0648ad
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-02 11:51:46 +0100

    point fix

--- point/main.go
+++ point/main.go
@@ -1,22 +1,49 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 type point struct {
 	x int
 	y int
 }
 
-func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
+func setPoint(p *point) {
+	p.x = 42
+	p.y = 21
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func printNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	var d []rune
+	for n > 0 {
+		d = append(d, rune(n%10+'0'))
+		n /= 10
+	}
+	for i := len(d) - 1; i >= 0; i-- {
+		z01.PrintRune(d[i])
+	}
 }
 
 func main() {
-	points := &point{}
+	p := &point{}
+	setPoint(p)
 
-	setPoint(points)
-
-	fmt.Printf("x = %d, y = %d", points.x, points.y)
-	fmt.Println()
+	printStr("x = ")
+	printNbr(p.x)
+	printStr(", y = ")
+	printNbr(p.y)
+	z01.PrintRune('\n')
 }
