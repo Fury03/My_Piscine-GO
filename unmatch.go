79dda06481284d4bf63e1df07d6593b79e521f85
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-04 14:45:57 +0100

    changed count cos of wrong counting

--- unmatch.go
+++ unmatch.go
@@ -1,14 +1,14 @@
 package student
 
 func Unmatch(a []int) int {
-	count := 0
 	for i := 0; i < len(a); i++ {
+		count := 0
 		for j := 0; j < len(a); j++ {
-			if i != j && a[i] == a[j] {
+			if a[i] == a[j] {
 				count++
 			}
 		}
-		if count == 0 || count%2 == 0 {
+		if count%2 != 0 {
 			return a[i]
 		}
 	}
