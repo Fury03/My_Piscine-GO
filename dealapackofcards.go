ffd756054390e5635bbea2495c9de3b85402a6d9
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-04 20:49:14 +0100

    deal2

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func DealAPackOfCards(deck []int) {
 	for i := 0; i < 4; i++ {
-		fmt.Printf("Player %d: %d, %d,%d\n", i+1, deck[i*3], deck[i*3+1], deck[i*3+2])
+		fmt.Printf("Player %d: %d, %d, %d\n", i+1, deck[i*3], deck[i*3+1], deck[i*3+2])
 	}
 }
