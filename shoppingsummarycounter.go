e738a3dad3316f3051c1705e293bd568c5bb9bec
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-04 18:48:13 +0100

    summary counter

---
+++ shoppingsummarycounter.go
@@ -0,0 +1,22 @@
+package student
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	summary := make(map[string]int)
+	word := ""
+
+	for i := 0; i < len(str); i++ {
+		if str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r' {
+			if word != "" {
+				summary[word]++
+			}
+			word = ""
+		} else {
+			word += string(str[i])
+		}
+	}
+	if word != "" {
+		summary[word]++
+	}
+
+	return summary
+}
