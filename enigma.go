b7f65ecb6cd07277401f9f12da6cdfb15db2a014
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-05 00:38:35 +0100

    enigma2

---
+++ enigma.go
@@ -0,0 +1,14 @@
+package student
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+
+	valueA := ***a
+	valueB := *b
+	valueC := *******c
+	valueD := ****d
+
+	***a = valueB
+	*b = valueD
+	*******c = valueA
+	****d = valueC
+}
