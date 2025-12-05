24a64c84f51d91c40aa25119fc6f9debc7d02afc
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-05 04:20:34 +0100

    reverse2

--- reversemenuindex.go
+++ reversemenuindex.go
@@ -1,11 +1,11 @@
 package student
 
 func ReverseMenuIndex(menu []string) []string {
-    l := len(menu)
-    reversed := make([]string, l)
+	l := len(menu)
+	reversed := make([]string, l)
 
-    for i := 0; i < l; i++ {
-        reversed[i] = menu[l-1-i]
-    }
-    return reversed
+	for i := 0; i < l; i++ {
+		reversed[i] = menu[l-1-i]
+	}
+	return reverse
 }
