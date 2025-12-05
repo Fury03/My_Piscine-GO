b0ebc22bbd5bebec739c6e2c5a9ac6d94177dd13
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-05 03:29:54 +0100

    comcheck trial 2

--- comcheck/main.go
+++ comcheck/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fot _,arg := range os.Args[1:] {
+	for _, arg := range os.Args[1:] {
 		if arg == "01" || arg == "galaxy" || arg == "galaxy 01" {
 			fmt.Println("Alert!!!")
 			return
