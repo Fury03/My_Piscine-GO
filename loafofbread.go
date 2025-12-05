ce4600b7601efdc631d69ab1e43f0fcac9e75319
Damilola Ogunrotimi <damilolaogunrotimi@gmail.com>
2025-12-05 11:47:11 +0100

    loaf another try2

--- loafofbread.go
+++ loafofbread.go
@@ -1,29 +1,55 @@
 package student
 
 func LoafOfBread(str string) string {
-	if len(str) == 0 {
+	length := 0
+	for _, r := range str {
+		if r != ' ' {
+			length++
+		}
+	}
+
+	if length == 0 {
 		return "\n"
 	}
-	if len(str) < 5 {
+
+	if length < 5 {
 		return "Invalid Output\n"
 	}
 
-	// Remove spaces for counting letters
-	letters := ""
-	for _, ch := range str {
-		if ch != ' ' {
-			letters += string(ch)
+	result := ""
+	word := ""
+	count := 0
+	skipNext := false
+
+	for _, r := range str {
+		if skipNext {
+			skipNext = false
+			continue
+		}
+		if r == ' ' {
+			continue
+		}
+
+		word += string(r)
+		count++
+
+		if count == 5 {
+			if result != "" {
+				result += " "
+			}
+			result += word
+			word = ""
+			count = 0
+			skipNext = true
 		}
 	}
 
-	returnStr := ""
-	for i, ch := range letters {
-		returnStr += string(ch)
-		// Add a space after every 5 letters
-		if (i+1)%5 == 0 && i != len(letters)-1 {
-			returnStr += " "
+	if word != "" {
+		if result != "" {
+			result += " "
 		}
+		result += word
 	}
 
-	return returnStr + "\n"
+	return result + "\n"
 }
